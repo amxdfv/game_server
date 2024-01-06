29ec1b6b9e80fc6a45df9b182cee562d9fc7433e
Лёшка <amxdfv@yandex.ru>
2024-01-06 18:09:42 +0300

    Новая реализация запуска приложения, плюс ограничение времени жизни соединения

--- main.py
+++ main.py
@@ -1,40 +1,46 @@
+import datetime
 import json
 
 import starlette.websockets
+import uvicorn
 from fastapi import FastAPI, WebSocket
-from logic import database_logic, response_logic
-from service_logic import logs_logic, websocket_logic
+from logic import database_logic, response_logic, config_logic
+from service_logic import logs_logic
+from threading import Thread
 import logging
 
 app = FastAPI()
 
-SESSIONS = {}
-
 
 @app.websocket("/gameserver")
-async def websocket_endpoint(websocket: WebSocket):  # TODO переделать запуск сокета, иначе менеджмент соединений не будет работать
-    income_client = websocket.client
+async def websocket_endpoint(
+        websocket: WebSocket):  # TODO переделать на нормальную тему, это какой-то пиздец
     logging.basicConfig(filename="logs//logs.log", level=20, encoding="UTF-8")
+    ttl = config_logic.read_config()
+
+    income_client = websocket.client
+    connection_time = datetime.datetime.now()
     try:
         await websocket.accept()
-        websocket_logic.add_client_to_list(websocket)
-        logging.info({income_client: 'Connected successfully'})
-        while True:
-            try:
-                data = await websocket.receive_text()
-                response = await processing_message(data)
-                await websocket.send_text(response)
-            except starlette.websockets.WebSocketDisconnect:
-                logging.info({income_client: "Disconnected"})
-            except Exception as inner_error:
-                await websocket.send_text(str(inner_error))
-                logging.error({income_client: str(inner_error)})
+        logging.info(logs_logic.construct_technical_log(income_client, 'Connected successfully'))
+        while (datetime.datetime.now() - connection_time).seconds < ttl["connection_lifetime"]:
+            if (datetime.datetime.now() - connection_time).seconds > ttl["connection_lifetime"]:
+                break
+            else:
+                try:
+                    data = await websocket.receive_text()
+                    response = await processing_message(data)
+                    await websocket.send_text(response)
+                except starlette.websockets.WebSocketDisconnect:
+                    logging.info(logs_logic.construct_technical_log(income_client, "Disconnected"))
+                except Exception as inner_error:
+                    await websocket.send_text(str(inner_error))
+                    logging.error(logs_logic.construct_technical_log(income_client, str(inner_error)))
+        await websocket.close()
     except starlette.websockets.WebSocketDisconnect:
-        logging.info({income_client: "Disconnected"})
+        logging.info(logs_logic.construct_technical_log(income_client, "Disconnected"))
     except Exception as error:
-        logging.error({income_client: str(error)})
-    finally:
-        websocket_logic.remove_client_from_list(websocket)
+        logging.error(logs_logic.construct_technical_log(income_client, str(error)))
 
 
 async def processing_message(msg):
@@ -52,3 +58,8 @@ async def processing_message(msg):
         response = response_logic.construct_response(json.loads(msg)["command"], "ERROR", str(error))
         logging.error(logs_logic.construct_log(json.loads(msg), response))
         return str(response)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(filename="logs//logs.log", level=20, encoding="UTF-8")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
--- service_logic/logs_logic.py
+++ service_logic/logs_logic.py
@@ -1,8 +1,15 @@
 import json
 import datetime
 
+import starlette.websockets
+
 
 def construct_log(inc_msg, out_msg):
     log_message = {'Timestamp': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'Incoming message': inc_msg,
                    'Outcoming message': out_msg}
+    return json.dumps(log_message)
+
+
+def construct_technical_log(client, msg):
+    log_message = {'Timestamp': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), str(client): msg}
     return json.dumps(log_message)
